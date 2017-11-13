fa698ebadbe2f753a91e3cb8a1b55af7fbde4f63
Matthew Gordon <laxredemption5525@gmail.com>
2017-11-13 11:09:47 -0700

    push

---
+++ src/components/StripeCheckout.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class StripeCheckout extends Component {
+  render() {
+    return(
+      <div>
+        Checkout with Stripe
+        <form>
+          
+        </form>
+      </div>
+    )
+  }
+}
+
+export default StripeCheckout
--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
+import StripeCheckout from './StripeCheckout'
+
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <StripeCheckout />
+      </div>
     );
   }
 }
